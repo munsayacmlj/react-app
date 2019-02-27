b3d40ca4d3318f717ad14e90f9ec8f038cab7a7e
Mark Munsayac <munsayacmlj@gmail.com>
2019-02-27 14:09:13 +0800

    Add update airtable function

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Modal from 'react-modal';
+import Airtable from 'airtable';
 import '../../styles/BookDetail.css';
 
 // Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
@@ -15,7 +16,37 @@ class BookDetail extends Component {
       bookAuthor: '',
       bookRating: '',
       bookImage: '',
-      bookDescription: ''
+      bookDescription: '',
+      bookId: '',
+      originalState: {}
+    }
+  }
+
+  componentDidMount = () => {
+    let { bookDetails } = this.props;
+    
+    if (Object.entries(bookDetails).length === 0 && bookDetails.constructor === Object) {
+      bookDetails = JSON.parse(localStorage.getItem("bookDetails"));
+      
+      this.setState({
+        bookTitle: bookDetails.title,
+        bookAuthor: bookDetails.author,
+        bookRating: bookDetails.rating,
+        bookImage: bookDetails.image,
+        bookDescription: bookDetails.description,
+        bookId: bookDetails.id,
+        originalState: bookDetails
+      });
+    } else {
+      this.setState({
+        bookTitle: bookDetails.title,
+        bookAuthor: bookDetails.author,
+        bookRating: bookDetails.rating,
+        bookImage: bookDetails.image,
+        bookDescription: bookDetails.description,
+        bookId: bookDetails.id,
+        originalState: bookDetails
+      });
     }
   }
 
@@ -24,43 +55,41 @@ class BookDetail extends Component {
   }
 
   closeModal = () => {
-    this.setState(this.baseState);
-    this.setState({modalIsOpen: false});
+    this.setState({
+      modalIsOpen: false,
+      bookTitle: this.state.originalState.title,
+      bookAuthor: this.state.originalState.author,
+      bookRating: this.state.originalState.rating,
+      bookDescription: this.state.originalState.description
+    });
   }
-
-  componentDidMount = () => {
-    let { bookDetails } = this.props;
-    if (Object.entries(bookDetails).length === 0 && bookDetails.constructor === Object) {
-      bookDetails = JSON.parse(localStorage.getItem("bookDetails"));
-
-      this.setState({
-        bookTitle: bookDetails.title,
-        bookAuthor: bookDetails.author,
-        bookRating: bookDetails.rating,
-        bookImage: bookDetails.image,
-        bookDescription: bookDetails.description,
-
-      });
-    } else {
-      this.setState({
-        bookTitle: bookDetails.title,
-        bookAuthor: bookDetails.author,
-        bookRating: bookDetails.rating,
-        bookImage: bookDetails.image,
-        bookDescription: bookDetails.description
-      });
-    }
-  }
-
+  
   handleChange = (e, state) => {
     this.setState({
       [state]: e.target.value 
     })
   }
 
-  render() {
-    const { bookTitle, bookAuthor, bookRating, bookImage, bookDescription } = this.state;
+  handleSubmit = (bookId, bookTitle, bookAuthor, bookRating, bookDescription) => {
+    var base = new Airtable({apiKey: 'key7D0jCBOHkuAmkv'}).base('appNcFuYsJ1L69Jt2');
     
+    base('Table 1').update(`${bookId}`, {
+      "title": `${bookTitle}`,
+      "image": "https://images.gr-assets.com/books/1520620414l/26032887.jpg",
+      "rating": `${bookRating}`,
+      "description": `${bookDescription}`,
+      "author": `${bookAuthor}`
+    }, function(err, record) {
+      if (err) { console.error(err); return; }
+      console.log(record.get('id'));
+    });
+
+    this.setState({ modalIsOpen: false });
+  }
+
+  render() {
+    const { bookTitle, bookAuthor, bookRating, bookImage, bookDescription, bookId } = this.state;
+
     return (
       <React.Fragment>
         <div className="book-detail">
@@ -83,24 +112,22 @@ class BookDetail extends Component {
         >
           <button className="close-modal" onClick={this.closeModal}>Close</button>
           <p><strong>Edit</strong></p>
-          <form method="POST">
-            <div className="book-modal-content__body">
-              <div className="book-modal-content__title">
-                <label>Title</label>
-                <input name="bookTitle" id="bookTitle" value={bookTitle} onChange={(e) => this.handleChange(e, 'bookTitle')}></input>
-              </div>
-              <div className="book-modal-content__author">
-                <label>Author</label>
-                <input name="bookAuthor" id="bookAuthor" value={bookAuthor} onChange={(e) => this.handleChange(e, 'bookAuthor')}></input>
-              </div>
-              <div className="book-modal-content__rating">
-                <label>Rating</label>
-                <input name="bookRating" id="bookRating" value={bookRating} onChange={(e) => this.handleChange(e, 'bookRating')}></input>
-              </div>
-              <textarea value={bookDescription} onChange={(e) => this.handleChange(e, 'bookDescription')} className="book-modal-content__description" rows="7" cols="70" name="bookDescription" id="bookDescription"></textarea>
-              <button type="submit">Submit</button>
+          <div className="book-modal-content__body">
+            <div className="book-modal-content__title">
+              <label>Title</label>
+              <input name="bookTitle" id="bookTitle" value={bookTitle} onChange={(e) => this.handleChange(e, 'bookTitle')}></input>
             </div>
-          </form>
+            <div className="book-modal-content__author">
+              <label>Author</label>
+              <input name="bookAuthor" id="bookAuthor" value={bookAuthor} onChange={(e) => this.handleChange(e, 'bookAuthor')}></input>
+            </div>
+            <div className="book-modal-content__rating">
+              <label>Rating</label>
+              <input name="bookRating" id="bookRating" value={bookRating} onChange={(e) => this.handleChange(e, 'bookRating')}></input>
+            </div>
+            <textarea value={bookDescription} onChange={(e) => this.handleChange(e, 'bookDescription')} className="book-modal-content__description" rows="7" cols="70" name="bookDescription" id="bookDescription"></textarea>
+            <button onClick={() => {this.handleSubmit(bookId, bookTitle, bookAuthor, bookRating, bookDescription)}}>Submit</button>
+          </div>
         </Modal>
       </React.Fragment>
     );
