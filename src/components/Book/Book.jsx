c179431262f24b7b73211ba1443d143dbed93852
Mark Munsayac <munsayacmlj@gmail.com>
2019-02-27 14:10:13 +0800

    Override book details id with id from airtable.

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -3,11 +3,6 @@ import { Link } from "react-router-dom";
 import '../../styles/Book.css';
 
 class Book extends Component {
-  
-  componentDidMount = () => {
-    console.log("Book did mount");
-  }
-  
 
   handleClick = (e, data) => {
     this.props.sendData(data);
@@ -19,15 +14,16 @@ class Book extends Component {
   }
 
   render() {
-    const { details, url } = this.props;
-    // const URLtitle = details.title.toLowerCase().split(' ').join('_');
+    const { details, url, id } = this.props;
+    details.id = id;
+    
     return (
       <div className="book">
         <div className="book__header">
           <h3 className="book__title">{details.title}</h3>
         </div>
         <div className="book__body">
-          <Link to={`${url}/${details.id}`} className="book__link" onClick={((e) => this.handleClick(e, details))}>
+          <Link to={`${url}/${id}`} className="book__link" onClick={((e) => this.handleClick(e, details))}>
             <img className="book__cover" src={details.image}></img>
           </Link>
           <p>
